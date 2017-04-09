8c3e23b88f06d00634f0f87a5d6233dc4da2ee20
gardenshade <some.shaded.garden@gmail.com>
2017-04-08 23:22:13 -0600

    minified compass css

--- build/compiled/js/Custom_Styles_for_MAM.user.js
+++ build/compiled/js/Custom_Styles_for_MAM.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         Custom Styles for MAM
 // @namespace    https://greasyfork.org/en/users/36444
-// @version      0.0.2
+// @version      0.0.3
 // @description  A custom style program
 // @author       GardenShade
 // @include      https://myanonamouse.net/*
 // @include      https://www.myanonamouse.net/*
+// @resource     customCSS https://raw.githubusercontent.com/gardenshade/mam-styles/master/build/compiled/css/test.css
+// @grant        GM_getResourceText
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_listValues
 // @grant        GM_deleteValue
 // @grant        GM_addStyle
 // ==/UserScript==;
-!function(n,e){var t,r,f;t=function(n){return n.disabled=!0},r=function(){var n,e,r;for(n=0,e=f.length;n<e;n++)r=f[n],r.href.indexOf("userCSS.php")<0&&t(r)},f=n.styleSheets,r()}(document,window);
+!function(e,o){var t,l,n;console.log("MAM Styles"),t=function(){var o,t,l,n,r;for(console.log("clearstyle"),r=e.head.querySelectorAll("link[rel=stylesheet]"),l=[],o=0,t=r.length;o<t;o++)n=r[o],n.title.indexOf("userCSS")<0&&n.href.indexOf("categories")<0&&l.push(n.parentNode.removeChild(n));return l},l=function(e){return console.log("mamStyles"),t(),GM_addStyle(e)},n=GM_getResourceText("customCSS"),l(n)}(document,window);
